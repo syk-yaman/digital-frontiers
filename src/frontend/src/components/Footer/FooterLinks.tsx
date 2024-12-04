d7fa1ea6cc548df61a287acb53ef38967041897a
Yaman Kalaji <syk.yaman@gmail.com>
2024-12-04 17:14:59 +0000

    Initial dataset webpage design

---
+++ src/frontend/src/components/Footer/FooterLinks.tsx
@@ -0,0 +1,89 @@
+import { IconBrandInstagram, IconBrandTwitter, IconBrandYoutube } from '@tabler/icons-react';
+import { ActionIcon, Container, Group, Text } from '@mantine/core';
+import { MantineLogo } from '@mantinex/mantine-logo';
+import classes from './FooterLinks.module.css';
+
+const data = [
+  {
+    title: 'About',
+    links: [
+      { label: 'Home', link: '#' },
+      { label: 'Data menu', link: '#' },
+      { label: 'Showcases', link: '#' },
+      { label: 'About', link: '#' },
+    ],
+  },
+  {
+    title: 'Project',
+    links: [
+      { label: 'Contribute', link: '#' },
+      { label: 'Media assets', link: '#' },
+      { label: 'Changelog', link: '#' },
+      { label: 'Releases', link: '#' },
+    ],
+  },
+  {
+    title: 'Community',
+    links: [
+      { label: 'Follow on Bluesky', link: '#' },
+      { label: 'Follow on Twitter', link: '#' },
+      { label: 'Email newsletter', link: '#' },
+      { label: 'GitHub discussions', link: '#' },
+    ],
+  },
+];
+
+export function FooterLinks() {
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<'a'>
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+        <Text fw={700} size="lg">Digital Frontiers</Text>
+
+          <Text size="xs" c="dimmed" className={classes.description}>
+            A DaaS platform for the Olympic Park
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © 2024 Digital Frontiers.  All rights reserved.
+        </Text>
+
+        <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
--- src/frontend/src/pages/Home.page.tsx
+++ src/frontend/src/pages/Home.page.tsx
@@ -1,11 +1,39 @@
+import { HeaderMegaMenu } from '@/components/Header/HeaderMegaMenu';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { FooterLinks } from '@/components/Footer/FooterLinks';
+import { Carousel } from '@mantine/carousel';
+import '@mantine/carousel/styles.css';
+import { Image } from '@mantine/core';
+
+// ✅ Types are available here
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { LatLngTuple } from 'leaflet';
+import { FeaturesGrid } from '@/components/FeaturesGrid/FeaturesGrid';
+
+const position: LatLngTuple = [51.505, -0.09];
 
 export function HomePage() {
   return (
     <>
-      <Welcome />
+      <HeaderMegaMenu />
+      <Carousel withIndicators height={500}> 
+        <Carousel.Slide>
+          <Image
+          src="https://placehold.co/1600x500/d0d0d0/FFF?text=Placeholder"
+        />
+        </Carousel.Slide>
+        <Carousel.Slide><Image
+          src="https://placehold.co/1600x500/d0d0d0/FFF?text=Placeholder"
+        /></Carousel.Slide>
+        <Carousel.Slide><Image
+          src="https://placehold.co/1600x500/d0d0d0/FFF?text=Placeholder"
+        /></Carousel.Slide>
+        {/* ...other slides */}
+      </Carousel>
+      <FeaturesGrid/>
       <ColorSchemeToggle />
+      <FooterLinks />
     </>
   );
 }
