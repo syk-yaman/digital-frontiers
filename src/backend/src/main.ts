ea74f3c63a6dd4aface69e2a63ddfaad12f0558b
Yaman Kalaji <syk.yaman@gmail.com>
2025-02-03 01:43:55 +0000

    Change API tilte

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -7,10 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Digital Frontiers API')
+    .setDescription('The Digital Frontiers API description')
     .setVersion('1.0')
-    .addTag('cats')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
