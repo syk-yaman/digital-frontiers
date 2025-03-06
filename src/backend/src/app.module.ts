ad0a614583d43cf8398dd1cd1e0958e853f92e9f
Yaman Kalaji <syk.yaman@gmail.com>
2025-03-06 02:11:50 +0000

    Add missing entities in app.module

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './modules/users/user.entity';
 import { UsersModule } from './modules/users/users.module';
 import { AppDataSource } from '../data-source';
+import { DatasetsModule } from './modules/datasets/datasets.module';
+import { Dataset, DatasetLink, DatasetLocation, DatasetSliderImage, DatasetTag } from './modules/datasets/dataset.entity';
 
 @Module({
   //imports: [TypeOrmModule.forRoot(AppDataSource.options)],
@@ -13,11 +15,13 @@ import { AppDataSource } from '../data-source';
     username: 'postgres',
     password: 'postgres',
     database: 'DigitalFrontiersDB',
-    entities: [User],
+    entities: [User, Dataset, DatasetLink, DatasetLocation, DatasetSliderImage, DatasetTag],
     synchronize: false, //to be off in production
     migrations: ['src/database/migrations/*-migration.ts'],
     migrationsTableName: '_migrations',
-  }), UsersModule],
+  }),
+    UsersModule,
+    DatasetsModule],
   controllers: [],
   providers: [],
 })
