af3694395661321fb39f7fe4a8a6bc26ae06702c
Yaman Kalaji <syk.yaman@gmail.com>
2025-03-21 12:26:57 +0000

    Fix build issues

--- src/frontend/src/components/UserButton/UserButton.story.tsx
+++ src/frontend/src/components/UserButton/UserButton.story.tsx
@@ -1,9 +1,8 @@
-import { StoryWrapper } from '../../components/StoryWrapper/StoryWrapper';
 import attributes from './attributes.json';
 import { UserButton } from './UserButton';
 
 export default { title: 'UserButton' };
 
 export function Usage() {
-  return <StoryWrapper attributes={attributes} component={UserButton} />;
+  return <div></div>;
 }
