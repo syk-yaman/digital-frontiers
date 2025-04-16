a29cec433930b4959aa0c6c493d251561cd24ff6
Yaman Kalaji <syk.yaman@gmail.com>
2025-04-16 02:53:23 +0100

    Move changelog to a seperate ts file

--- src/frontend/src/components/Header/HeaderMegaMenu.tsx
+++ src/frontend/src/components/Header/HeaderMegaMenu.tsx
@@ -38,101 +38,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '@/context/AuthContext';
 import classes from './HeaderMegaMenu.module.css';
-
-const version = 'v0.2.5'
-const changelog = [
-  {
-    version: 'v0.2.5',
-    changes: [
-      'Users can sign up and sign in now.',
-      'Admin can sign in and access the admin page (permissons are acitvated).',
-      'Bug fix: park boundaries were not showing on the map.',
-      'Changed user types in sign up page',
-      'Bug fix: tag duplication and tag suggestion in the add wizard page',
-      'Terms and conditions page was added',
-      'Now the wizard jumps to step 4 if the dataset is static',
-      'Several explanations/highlights were added in the wizard',
-    ],
-  },
-  {
-    version: 'v0.2.4',
-    changes: [
-      'Bug fixes in dataset add wizrd',
-      'Bug fixes in homepage',
-      'MQTT link validation feature was added',
-      'Changed map mode to satellite',
-    ],
-  },
-  {
-    version: 'v0.2.3',
-    changes: [
-      'Bug fixes in dataset add wizrd',
-      'Admin page is capable of deleting datasets now',
-    ],
-  },
-  {
-    version: 'v0.2.2',
-    changes: [
-      'Major fixes for the dataset add page',
-      'Backend architecture upgrade',
-      'Admin page skelaton was added'
-    ],
-  },
-  {
-    version: 'v0.2.1',
-    changes: [
-      'Add dataset page is working',
-      'Backend is available now',
-    ],
-  },
-  {
-    version: 'v0.1.5',
-    changes: [
-      'Updated the signup page according to feedback.',
-      'Major update to the dataset-add wizard including feedback implementation',
-      'Activated the QEOP boundary on the map using GIS plugins.',
-      'Minor fixes.'
-    ],
-  },
-  {
-    version: 'v0.1.4',
-    changes: [
-      'Activated the list view in the data menu.',
-      'Added the login / sign up pages.',
-      'Added the dataset add wizrd.',
-      'Font is set to bold in the navbar.'
-    ],
-  },
-  {
-    version: 'v0.1.3',
-    changes: [
-      'Minor fixes for Homepage after QA.',
-    ],
-  },
-  {
-    version: 'v0.1.2',
-    changes: [
-      'Major upgrade for the homepage.',
-      'Update the main theme according to Shift brand guidlines.',
-      'New overlays to the hero image in homepage with new high quality photo.',
-      'Version number is added to the homepage with changelog dialog.',
-      'Homepage has been filled with showable real data.',
-      'Dataset page updated with real content (click on "Bats Activity in the QEOP" in homepage)',
-    ],
-  },
-  {
-    version: 'v0.1.1',
-    changes: [
-      'Minor style fixes for the homepage.',
-    ],
-  },
-  {
-    version: 'v0.1.0',
-    changes: [
-      'Initial version.',
-    ],
-  },
-];
+import { changelog, version } from './changelog';
 
 const mockdata = [
   {
@@ -418,7 +324,7 @@ export function HeaderMegaMenu() {
             )}
           </Group>
 
-          <Burger opened={drawerOpened} onClick={toggleDrawer} hiddenFrom="sm" />
+          <Burger opened={drawerOpened} onClick={toggleDrawer} />
         </Group>
       </header>
       <Modal opened={modalOpened} onClose={closeModal} c='#FFC747' className={classes.modalcustom} // Use the custom class here
@@ -447,6 +353,7 @@ export function HeaderMegaMenu() {
         title="Navigation"
         hiddenFrom="sm"
         zIndex={1000000}
+        position="right"
       >
         <ScrollArea h="calc(100vh - 80px)" mx="-md">
           <Divider my="sm" />
---
+++ src/frontend/src/components/Header/changelog.ts
@@ -0,0 +1,94 @@
+export const version = 'v0.2.5'
+export const changelog = [
+    {
+        version: 'v0.2.5',
+        changes: [
+            'Users can sign up and sign in now.',
+            'Admin can sign in and access the admin page (permissions are activated).',
+            'Bug fix: park boundaries were not showing on the map.',
+            'Changed user types in sign-up page.',
+            'Bug fix: tag duplication and tag suggestion in the add wizard page.',
+            'Terms and conditions page was added.',
+            'Now the wizard jumps to step 4 if the dataset is static.',
+            'Several explanations/highlights were added in the wizard.',
+        ],
+    },
+    {
+        version: 'v0.2.4',
+        changes: [
+            'Bug fixes in dataset add wizard.',
+            'Bug fixes in homepage.',
+            'MQTT link validation feature was added.',
+            'Changed map mode to satellite.',
+        ],
+    },
+    {
+        version: 'v0.2.3',
+        changes: [
+            'Bug fixes in dataset add wizard.',
+            'Admin page is capable of deleting datasets now.',
+        ],
+    },
+    {
+        version: 'v0.2.2',
+        changes: [
+            'Major fixes for the dataset add page.',
+            'Backend architecture upgrade.',
+            'Admin page skeleton was added.',
+        ],
+    },
+    {
+        version: 'v0.2.1',
+        changes: [
+            'Add dataset page is working.',
+            'Backend is available now.',
+        ],
+    },
+    {
+        version: 'v0.1.5',
+        changes: [
+            'Updated the signup page according to feedback.',
+            'Major update to the dataset-add wizard including feedback implementation.',
+            'Activated the QEOP boundary on the map using GIS plugins.',
+            'Minor fixes.',
+        ],
+    },
+    {
+        version: 'v0.1.4',
+        changes: [
+            'Activated the list view in the data menu.',
+            'Added the login / sign-up pages.',
+            'Added the dataset add wizard.',
+            'Font is set to bold in the navbar.',
+        ],
+    },
+    {
+        version: 'v0.1.3',
+        changes: [
+            'Minor fixes for Homepage after QA.',
+        ],
+    },
+    {
+        version: 'v0.1.2',
+        changes: [
+            'Major upgrade for the homepage.',
+            'Update the main theme according to Shift brand guidelines.',
+            'New overlays to the hero image in homepage with new high-quality photo.',
+            'Version number is added to the homepage with changelog dialog.',
+            'Homepage has been filled with showable real data.',
+            'Dataset page updated with real content (click on "Bats Activity in the QEOP" in homepage).',
+        ],
+    },
+    {
+        version: 'v0.1.1',
+        changes: [
+            'Minor style fixes for the homepage.',
+        ],
+    },
+    {
+        version: 'v0.1.0',
+        changes: [
+            'Initial version.',
+        ],
+    },
+];
